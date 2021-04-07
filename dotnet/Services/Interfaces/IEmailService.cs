a86b379d89a26117b5c60e417a122e3217f5d0e9
Ledwyn Mena <77592899+LedwynMena@users.noreply.github.com>
2021-04-07 14:09:41 -0400

    Update IEmailService.cs

--- dotnet/Services/Interfaces/IEmailService.cs
+++ dotnet/Services/Interfaces/IEmailService.cs
@@ -1,20 +1,9 @@
-﻿using Sabio.Models.Requests;
-using Sabio.Models.Requests.Email;
-using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Sabio.Services.Interfaces
+namespace Services.Interfaces
 {
    public interface IEmailService
     {
         Task SendEmail(SendGridMessage msg);
-
-        Task SampleEmail(EmailAddRequest email);
-
-        Task SendRegistrationConfirmation(string email, string firstName, string token);
 
         Task SendPasswordResetEmail(string email, string token);
 
