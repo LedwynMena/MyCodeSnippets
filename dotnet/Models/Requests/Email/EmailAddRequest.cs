7fdfa224636033df3f31d165ea6d5cd8f35a58fe
Ledwyn Mena <77592899+LedwynMena@users.noreply.github.com>
2021-04-07 14:12:42 -0400

    Update EmailAddRequest.cs

--- dotnet/Models/Requests/Email/EmailAddRequest.cs
+++ dotnet/Models/Requests/Email/EmailAddRequest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace Sabio.Models.Requests.Email
+namespace Models.Requests.Email
 {
     public class EmailAddRequest
     {
