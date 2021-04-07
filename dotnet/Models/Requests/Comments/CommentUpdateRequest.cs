0a93c6e4a302b232e1353c6ec102976106adfcd2
Ledwyn Mena <77592899+LedwynMena@users.noreply.github.com>
2021-04-07 14:12:25 -0400

    Update CommentUpdateRequest.cs

--- dotnet/Models/Requests/Comments/CommentUpdateRequest.cs
+++ dotnet/Models/Requests/Comments/CommentUpdateRequest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Sabio.Models.Requests.Comments
+namespace Models.Requests.Comments
 {
     public class CommentUpdateRequest : CommentAddRequest, IModelIdentifier
     {
