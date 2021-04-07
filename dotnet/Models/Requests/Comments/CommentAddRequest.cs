a75dd0c37964457fd9de9a72d8f80a674a31a538
Ledwyn Mena <77592899+LedwynMena@users.noreply.github.com>
2021-04-07 14:12:11 -0400

    Update CommentAddRequest.cs

--- dotnet/Models/Requests/Comments/CommentAddRequest.cs
+++ dotnet/Models/Requests/Comments/CommentAddRequest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace Sabio.Models.Requests.Comments
+namespace Models.Requests.Comments
 {
     public class CommentAddRequest
     {
